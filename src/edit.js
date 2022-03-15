439a702ebaa5d56e12fd21b9cc805e01aba87d02
Seth Miller <seth@flexperception.com>
2022-03-15 15:59:38 -0400

    Attributes working in Edit component

--- src/edit.js
+++ src/edit.js
@@ -25,7 +25,8 @@ import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody } from '@wordpress/components'
 
 import UserSelector from './UserSelector.js'
-
+import { useState, useEffect } from '@wordpress/element'
+import apiFetch from '@wordpress/api-fetch'
 
 /**
  * The edit function describes the structure of your block in the context of the
@@ -46,6 +47,55 @@ const Edit = ( props ) => {
 		attributes: { isLoaded, user, users }
 	} = props
 
+	console.log(props)
+
+	props.setAttributes ( {isLoaded: true})
+	console.log(props.attributes.isLoaded)
+
+
+	const [error, setError] = useState(null);
+    // const [featuredUser, setFeaturedUser] = useState()
+    // const [users, setUsers] = useState([])
+    // const [isLoaded, setIsLoaded] = useState(false)
+
+	// Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect( (props) => {        
+        apiFetch( { path: '/wp/v2/users/me' } )
+        .then ( 
+            ( props ) => {
+                // console.log("isLoaded: ", props.attributes.isLoaded)
+                // setIsLoaded(true)
+                console.log("Before setUser function: ", props.user)
+                // setUser(props.user)
+                console.log("After setUser function: ", props.user)
+                console.log("Display name: ", props.user.name)
+                console.log("Bio: ", props.user.description)
+                console.log("Email: ", user.user_email)
+            }, 
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                setError(error)
+                setIsLoaded(true)
+                // props.setAttributes( {isLoaded: true} )
+                // setAttributes( {isLoaded: true} )
+                console.log("ERROR: ", error    )
+            }
+        );
+    }, [])
+
+	function onChangeInput(props, newInput) {
+		console.log(props)
+		props.setAttributes({isLoaded: true})
+	}
+
+	const changeSetAttributes = () => { 
+		attributes.setAttributes( {isLoaded: true} )
+	}
+
 	return (
 
 		<div {...useBlockProps()}>
@@ -54,7 +104,10 @@ const Edit = ( props ) => {
 			<InspectorControls>
 				<PanelBody title={__("Select User")}>
 					{/* do stuff */}
-					<UserSelector props={props} />
+					{/* <UserSelector props={props} /> */}
+					<input onChange={ changeSetAttributes } type="text" />
+
+					
 				</PanelBody>
 			</InspectorControls>
 		}
