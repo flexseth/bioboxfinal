2ef2f239403850891b1514b41b6f4ee21787d3a4
Seth Miller <seth@flexperception.com>
2022-03-16 15:02:26 -0400

    Fixed loading component, set attributes before set load.. duh

--- src/edit.js
+++ src/edit.js
@@ -29,6 +29,13 @@ import { useState, useEffect } from '@wordpress/element'
 import apiFetch from '@wordpress/api-fetch'
 import Gravatar from 'react-gravatar'
 
+import {
+    SlotFillProvider,
+    Slot,
+    Fill,
+    Panel,
+} from '@wordpress/components';
+
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
@@ -39,6 +46,9 @@ import Gravatar from 'react-gravatar'
  */
 const Edit = ( props ) => {
 
+	// set a timeout so the REST API can load users....
+	// this should resolve all issues
+
 	///////////////////////////////////////
 
 	const blockProps = useBlockProps();
@@ -46,62 +56,19 @@ const Edit = ( props ) => {
 		attributes: { isLoaded, user, users }
 	} = props
 
-	// console.log(props)
-
-	////////////////////// 
-	// WORKING BLOCK PROPS
-
-	const onChangeUser = (newUser) => {
-		console.log("[onChangeUser] user === : ", props.attributes.user)	
-		props.setAttributes( {user: newUser})
-		console.log("[onChangeUser] after set attributes====: ", props.attributes.user)
-	}
-
-	function onChangeInput(props, newInput) {
-		console.log(props)
-		props.setAttributes({isLoaded: true})
-	}
-
-	const changeSetAttributes = () => { 
-		props.setAttributes( {isLoaded: true} )
-		console.log("isLoaded: ", props.attributes.isLoaded)
-	}
-
-	// output of component
-	function bio(user) {
-		return (
-			<section props={props}>
-					<Gravatar email={props.attributes.user.user_email} size={150} />
-					<h2>{ props.attributes.user.name}</h2>
-					<p>{ props.attributes.user.description }</p>
-				</section> 	
-		)
-	}
-
-
-
+	// Fetch data from users endpoint
 	const [error, setError] = useState(null);
-    // const [featuredUser, setFeaturedUser] = useState()
-    // const [users, setUsers] = useState([])
-    // const [isLoaded, setIsLoaded] = useState(false)
 
 	// Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
     useEffect( (user) => {        
-        
 		apiFetch( { path: '/wp/v2/users/me' } )
         .then ( 
             ( user ) => {
-				// props.setAttributes( {isLoaded: true} )
 				props.setAttributes( {user: user} )
-
+				console.log("user.....", user)
 	
-				console.log( props.attributes.isLoaded )
-                console.log( props )
-                console.log("Display name: ", user.name)
-                console.log("Bio: ", user.description)
-                console.log("Email: ", user.user_email)
             }, 
             // Note: it's important to handle errors here
             // instead of a catch() block so that we don't swallow
@@ -114,57 +81,101 @@ const Edit = ( props ) => {
                 console.log("ERROR: ", error    )
             }
         );
-    }, [])	
+    }, [])
 
 	useEffect( (users) => {
-		apiFetch( { path: '/wp/v2/users?roles=author,editor,administrator' } )
-		.then ( 
-			(users) => {
-				props.setAttributes( {isLoaded: true} )
-				props.setAttributes( {users: users} )
-				console.log("Other users", users)
-			}, 
-			// Note: it's important to handle errors here
-			// instead of a catch() block so that we don't swallow
-			// exceptions from actual bugs in components.
-			(error) => {
-				props.setAttributes( {isLoaded: true} )
-				setError(error)
-			}             
-		);
-	}, [])
+			apiFetch( { path: '/wp/v2/users?roles=author,editor,administrator' } )
+			.then ( 
+				(users) => {
+					props.setAttributes( {users: users} )
+					props.setAttributes( {isLoaded: true} )
 
-	{console.log("[attributes]: ", props.attributes)}
-	{console.log("[user]: ", props.attributes.user)}
+					console.log("users......", users)
+				}, 
+				// Note: it's important to handle errors here
+				// instead of a catch() block so that we don't swallow
+				// exceptions from actual bugs in components.
+				(error) => {
+					props.setAttributes( {isLoaded: true} )
+					setError(error)
+				}             
+			);
+	}, [])	
+
+	// {console.log("[attributes]: ", props.attributes)}
+	// {console.log("[user]: ", props.attributes.user)}
 	
 	if (error) {
 		return <div>Error: {error.message}</div>
 	}
-	else if (!props.attributes.isLoaded) {
+	else if (!props.attributes.isLoaded ) {
 		return <div>Loading...</div>
 	}
-	
-	else { // API loaded, return fields (otherwise it throws errors)
-		
-		return (
-		<div {...blockProps}>
-		
-		{ /* Block Settings */ } 
-		{
-			<InspectorControls>
-				<PanelBody title={__("Select User")}>
-					
-					
-					
-				</PanelBody>
-			</InspectorControls>
-		}	
 
-		{ /* Block Editor Display */ } 
-			<Gravatar email={props.attributes.user.user_email} size={150} />
-			<h2>{ props.attributes.user.name}</h2>
-			<p>{ props.attributes.user.description }</p>
-			</div> 
+	
+	else { 
+		const bio = props.attributes.user // shorten access to variable
+		// API loaded, return bio box (otherwise it throws a damn fit)
+		// trying to display data in the DOM before it was loaded - it exploded
+		// trying to display data in the console before it was loaded, it just said undefined
+		return (
+			<div {...blockProps}>
+		
+				{ /* Block Settings */ } 
+			
+					<InspectorControls props={props}>
+						<PanelBody title={__("Select User")}>
+				
+					{/* {console.log("From panel body.........")}
+							{ console.log(props) } */}
+							
+								{/* <select 
+									props={props}
+									onChange={ handleChangeUser }>
+									{ 
+										props.attributes.users.map( ( user ) => (
+										<option 
+											key={ user.id }
+											>
+											{ user.name }
+										</option>
+										))
+									}
+								</select> */}
+
+
+						</PanelBody>
+					</InspectorControls>
+				{ /* end block settings */}
+
+
+				{ /* Block Editor Display */ } 
+
+						{/* {
+							(props) => {
+								if(!props.attributes.isLoaded) {
+									return <p>Loading...</p>
+								}
+								else {
+									return (
+										<div className="bio-box">
+											<Gravatar email={props.attributes.user.user_email} size={150} />
+											<h2>{ props.attributes.user.name}</h2>
+											<p>{ props.attributes.user.description }</p>
+										</div>
+									)
+								}
+							}
+						} */}
+
+
+						<div className="bio-box">
+							<Gravatar email={bio.user_email} size={150} />
+							<h2>{ bio.name}</h2>
+							<p>{ bio.description }</p>
+						</div>
+
+			</div> // end <div ...blockProps>
 			
 		);
 	}
