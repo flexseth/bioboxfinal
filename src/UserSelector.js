9b18844e318cdef99e9619039dc6d87007bf52c9
Seth Miller <seth@flexperception.com>
2022-09-27 11:07:18 -0400

    Code formatting added (Prettier)

--- src/UserSelector.js
+++ src/UserSelector.js
@@ -1,108 +1,95 @@
-import { useState, useEffect } from '@wordpress/element'
-import apiFetch from '@wordpress/api-fetch'
-import { useBlockProps } from '@wordpress/block-editor'
+import { useState, useEffect } from "@wordpress/element";
+import apiFetch from "@wordpress/api-fetch";
+import { useBlockProps } from "@wordpress/block-editor";
 
-const UserSelector = ( props ) => { 
-    //const props = useBlockProps()
+const UserSelector = (props) => {
+  //const props = useBlockProps()
 
-    // const {
-    //     attributes: {
-    //         user,
-    //         users
-    //     } 
-    // } = props;
+  // const {
+  //     attributes: {
+  //         user,
+  //         users
+  //     }
+  // } = props;
 
-    const [error, setError] = useState(null);
-    const [user, setUser] = useState()
-    const [users, setUsers] = useState([])
-    const [isLoaded, setIsLoaded] = useState(false)
+  const [error, setError] = useState(null);
+  const [user, setUser] = useState();
+  const [users, setUsers] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    console.log("Props: ", props)
+  console.log("Props: ", props);
 
-    const handleChangeUser = ( props, newUser ) => {
-    alert("Hello from handleChangeUser")
+  const handleChangeUser = (props, newUser) => {
+    alert("Hello from handleChangeUser");
 
-        try {
-            props.setAttributes( {user: newUser} );   
-        } catch (error) {
-            console.log("ERROR: ", error)
-        }
-        
+    try {
+      props.setAttributes({ user: newUser });
+    } catch (error) {
+      console.log("ERROR: ", error);
     }
+  };
 
-    // Note: the empty deps array [] means
-    // this useEffect will run once
-    // similar to componentDidMount()
-    useEffect( () => {        
-        apiFetch( { path: '/wp/v2/users/me' } )
-        .then ( 
-            ( user ) => {
-                // console.log("isLoaded: ", props.attributes.isLoaded)
-                // setIsLoaded(true)
-                console.log("Before setUser function: ", user)
-                setUser(user)
-                console.log("After setUser function: ", user)
-                console.log("Display name: ", user.name)
-                console.log("Bio: ", user.description)
-                console.log("Email: ", user.user_email)
-            }, 
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-                setError(error)
-                setIsLoaded(true)
-                // props.setAttributes( {isLoaded: true} )
-                // setAttributes( {isLoaded: true} )
-                console.log("ERROR: ", error    )
-            }
-        );
-    }, [])
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    apiFetch({ path: "/wp/v2/users/me" }).then(
+      (user) => {
+        // console.log("isLoaded: ", props.attributes.isLoaded)
+        // setIsLoaded(true)
+        console.log("Before setUser function: ", user);
+        setUser(user);
+        console.log("After setUser function: ", user);
+        console.log("Display name: ", user.name);
+        console.log("Bio: ", user.description);
+        console.log("Email: ", user.user_email);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setError(error);
+        setIsLoaded(true);
+        // props.setAttributes( {isLoaded: true} )
+        // setAttributes( {isLoaded: true} )
+        console.log("ERROR: ", error);
+      }
+    );
+  }, []);
 
-    useEffect( (users) => {
-            apiFetch( { path: '/wp/v2/users?roles=author,editor,administrator' } )
-            .then ( 
-                (users) => {
-                    setIsLoaded(true)
-                    setUsers(users)
-                    console.log("Other users", users)
-                }, 
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                    setIsLoaded(true)
-                    setError(error)
-                }             
-            );
-    }, [])
-    
-  
-    if (error) {
-    console.log("ERROR: ", error)
-      return <div>Error: {error.message}</div>;
-    } 
-    else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } 
-    
-    else {
+  useEffect((users) => {
+    apiFetch({ path: "/wp/v2/users?roles=author,editor,administrator" }).then(
+      (users) => {
+        setIsLoaded(true);
+        setUsers(users);
+        console.log("Other users", users);
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    );
+  }, []);
 
-    console.log("Before render, users:", users)
+  if (error) {
+    console.log("ERROR: ", error);
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    console.log("Before render, users:", users);
 
-      return (
-            <select
-                onChange={ handleChangeUser }>
-            {users.map(user => (
-                <option 
-                    key={user.id}>
-                    {user.name} 
-                </option>
-            ))}
-            </select>
-        );
-    }
-      
-}
+    return (
+      <select onChange={handleChangeUser}>
+        {users.map((user) => (
+          <option key={user.id}>{user.name}</option>
+        ))}
+      </select>
+    );
+  }
+};
 
-export default UserSelector
+export default UserSelector;
--- src/save.js
+++ src/save.js
@@ -5,7 +5,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-i18n/
  */
-import { __ } from '@wordpress/i18n';
+import { __ } from "@wordpress/i18n";
 
 /**
  * React hook that is used to mark the block wrapper element.
@@ -13,10 +13,10 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps } from "@wordpress/block-editor";
 
 /** External dependencies */
-import Gravatar from 'react-gravatar'
+import Gravatar from "react-gravatar";
 
 /**
  * The save function defines the way in which the different attributes should
@@ -27,28 +27,22 @@ import Gravatar from 'react-gravatar'
  *
  * @return {WPElement} Element to render.
  */
-const save = ( props ) => {
+const save = (props) => {
+  const blockProps = useBlockProps.save();
 
-	const blockProps = useBlockProps.save();
+  const {
+    attributes: { isLoaded, user, users },
+  } = props;
 
-	const {
-		attributes: {
-			isLoaded, user, users
-		}
-	} = props
+  return (
+    <div {...blockProps}>
+      <Gravatar email={props.attributes.user.user_email} size={150} />
+      <h2>{__(props.attributes.user.name, "bioboxfinal")}</h2>
+      <p>{__(props.attributes.user.description, "bioboxfinal")}</p>
 
-	return (
+      {/* "Good enough for 'government work' " -Mom & Dad */}
+    </div>
+  );
+};
 
-		<div {...blockProps} >
-
-			<Gravatar email={props.attributes.user.user_email} size={150} />
-			<h2>{ __( props.attributes.user.name, "bioboxfinal") }</h2>
-			<p>{ __( props.attributes.user.description, "bioboxfinal") }</p>
-
-			{/* "Good enough for 'government work' " -Mom & Dad */}
-		</div>
-	);
-
-}
-
-export default save
+export default save;
